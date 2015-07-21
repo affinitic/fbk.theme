a4b6174cfae26083e132ecb4ca01d73605772777
Martin Peeters <martin.peeters@affinitic.be>
2015-07-21 20:38:29 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""Installer for the fbk.theme package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    + '\n')
+
+
+setup(
+    name='fbk.theme',
+    version='0.1',
+    description="FBK website theme",
+    long_description=long_description,
+    # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3.6",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='Python Plone',
+    author='Martin Peeters',
+    author_email='martin.peeters@affinitic.be',
+    url='http://pypi.python.org/pypi/fbk.theme',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['fbk'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'plone.api',
+        'setuptools',
+        'z3c.jbot',
+        'plone.app.theming',
+        'plone.app.themingplugins',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'plone.app.contenttypes',
+            'plone.app.robotframework[debug]',
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
