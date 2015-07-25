096e8f115d8e871696db124be1d2f891a9226de2
Martin Peeters <martin.peeters@affinitic.be>
2015-07-25 19:37:30 +0200

    Customize the portal footer viewlet

---
+++ src/fbk/theme/browser/viewlets.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+fbk.theme
+---------
+
+Created by mpeeters
+:copyright: (c) 2015 by Affinitic SPRL
+:license: GPL, see LICENCE.txt for more details.
+"""
+
+from plone.app.layout.viewlets import common
+from plone.app.layout.navigation.interfaces import INavigationRoot
+
+
+class FooterViewlet(common.FooterViewlet):
+
+    def on_homepage(self):
+        """Verifiy if we are on the homepage"""
+        if INavigationRoot.providedBy(self.context):
+            return True
+        return False
