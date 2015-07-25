ceb98082dfb28856baee4996c3d6f4a424d99586
Martin Peeters <martin.peeters@affinitic.be>
2015-07-25 19:33:10 +0200

    Override the language selector viewlet to display only languages codes

--- src/fbk/theme/interfaces.py
+++ src/fbk/theme/interfaces.py
@@ -2,7 +2,8 @@
 """Module where all interfaces, events and exceptions live."""
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from plone.app.multilingual.interfaces import IPloneAppMultilingualInstalled
 
 
-class IFbkThemeLayer(IDefaultBrowserLayer):
+class IFbkThemeLayer(IPloneAppMultilingualInstalled, IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
