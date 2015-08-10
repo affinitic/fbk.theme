b65cd10d18ce3fd528f95aa5dcd99819340bb93d
Martin Peeters <martin.peeters@affinitic.be>
2015-08-10 21:32:05 +0200

    Fix the homepage viewlets

--- src/fbk/theme/browser/viewlets.py
+++ src/fbk/theme/browser/viewlets.py
@@ -10,13 +10,16 @@ Created by mpeeters
 
 from plone.app.layout.viewlets import common
 from plone.app.layout.navigation.interfaces import INavigationRoot
+from zope.component import getMultiAdapter
 
 
 class FooterViewlet(common.FooterViewlet):
 
     def on_homepage(self):
         """Verifiy if we are on the homepage"""
-        if INavigationRoot.providedBy(self.context):
+        context_helper = getMultiAdapter((self.context, self.request),
+                                         name="plone_context_state")
+        if INavigationRoot.providedBy(context_helper.canonical_object()):
             return True
         return False
 
@@ -25,6 +28,8 @@ class HomepageButtonsViewlet(common.ViewletBase):
 
     def on_homepage(self):
         """Verifiy if we are on the homepage"""
-        if INavigationRoot.providedBy(self.context):
+        context_helper = getMultiAdapter((self.context, self.request),
+                                         name="plone_context_state")
+        if INavigationRoot.providedBy(context_helper.canonical_object()):
             return True
         return False
