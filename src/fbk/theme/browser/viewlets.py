3fe083d71614ffe3d5f0cf531bb292c27cd9215e
Martin Peeters <martin.peeters@affinitic.be>
2015-08-24 18:32:10 +0200

    Use registry to store links on homepage / footer

--- src/fbk/theme/browser/viewlets.py
+++ src/fbk/theme/browser/viewlets.py
@@ -8,12 +8,37 @@ Created by mpeeters
 :license: GPL, see LICENCE.txt for more details.
 """
 
-from plone.app.layout.viewlets import common
+from plone import api
 from plone.app.layout.navigation.interfaces import INavigationRoot
+from plone.app.layout.viewlets import common
+from plone.registry.interfaces import IRegistry
 from zope.component import getMultiAdapter
+from zope.component import getUtility
+
+from fbk.theme.interfaces import IFBKSettings
 
 
-class FooterViewlet(common.FooterViewlet):
+class BaseViewlet(object):
+
+    @property
+    def root_url(self):
+        return api.portal.get_navigation_root(self.context).absolute_url()
+
+    @property
+    def links(self):
+        values = getUtility(IRegistry).forInterface(IFBKSettings)
+        portal_state = getMultiAdapter((self.context.aq_inner, self.request),
+                                       name=u'plone_portal_state')
+        lang = portal_state.language()
+        keys = (
+            'link_about',
+            'link_find',
+            'link_become',
+        )
+        return {k: getattr(values, '{0}_{1}'.format(k, lang)) for k in keys}
+
+
+class FooterViewlet(BaseViewlet, common.FooterViewlet):
 
     def on_homepage(self):
         """Verifiy if we are on the homepage"""
@@ -24,7 +49,7 @@ class FooterViewlet(common.FooterViewlet):
         return False
 
 
-class HomepageButtonsViewlet(common.ViewletBase):
+class HomepageButtonsViewlet(BaseViewlet, common.ViewletBase):
 
     def on_homepage(self):
         """Verifiy if we are on the homepage"""
--- src/fbk/theme/interfaces.py
+++ src/fbk/theme/interfaces.py
@@ -1,9 +1,59 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
-from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from plone.app.multilingual.interfaces import IPloneAppMultilingualInstalled
+from zope import schema
+from zope.interface import Interface
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
 class IFbkThemeLayer(IPloneAppMultilingualInstalled, IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class IFBKSettings(Interface):
+
+    link_about_fr = schema.TextLine(
+        title=u'About Kinesiology link (FR)',
+        required=True,
+    )
+
+    link_about_en = schema.TextLine(
+        title=u'About Kinesiology link (EN)',
+        required=True,
+    )
+
+    link_about_nl = schema.TextLine(
+        title=u'About Kinesiology link (NL)',
+        required=True,
+    )
+
+    link_find_fr = schema.TextLine(
+        title=u'Find Kinesiologist link (FR)',
+        required=True,
+    )
+
+    link_find_en = schema.TextLine(
+        title=u'Find Kinesiologist link (EN)',
+        required=True,
+    )
+
+    link_find_nl = schema.TextLine(
+        title=u'Find Kinesiologist link (NL)',
+        required=True,
+    )
+
+    link_become_fr = schema.TextLine(
+        title=u'Become Kinesiologist link (FR)',
+        required=True,
+    )
+
+    link_become_en = schema.TextLine(
+        title=u'Become Kinesiologist link (EN)',
+        required=True,
+    )
+
+    link_become_nl = schema.TextLine(
+        title=u'Become Kinesiologist link (NL)',
+        required=True,
+    )
